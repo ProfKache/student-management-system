82ba70d24743d610a9ffbd3e14676d12983cd261
Salim Kachemela <sakachemela@gmail.com>
2024-04-08 12:05:25 +0300

    Initial Commit

---
+++ src/test/java/com/profkache/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profkache.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
