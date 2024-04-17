f5ca9ddde767950e22a2f4e9261f254d30c3ed78
Salim Kachemela <sakachemela@gmail.com>
2024-04-17 18:54:08 +0300

    Add methods for update and delete

--- src/main/java/com/profkache/sms/controller/StudentController.java
+++ src/main/java/com/profkache/sms/controller/StudentController.java
@@ -6,11 +6,8 @@ import com.profkache.sms.entity.Student;
 import com.profkache.sms.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Controller
 public class StudentController {
@@ -46,6 +43,33 @@ public class StudentController {
     @PostMapping("/students")
     public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable("id") Long id,
+                                @ModelAttribute("student") Student student) {
+
+        Student existingStudent = studentService.getStudentById(id);
+
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        studentService.updateStudent(existingStudent);
+
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/{id}")
+    public String deleteStudent(@PathVariable("id") Long id) {
+        studentService.deleteStudent(id);
         return "redirect:/students";
     }
 }
